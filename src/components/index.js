a2cf373e83c15fbd79a2c85a50cd4f456a34d84c
Rana Zohaib <zohaib.dilshad.1@gmail.com>
2023-11-20 13:22:22 +0500

    setup routes

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Quiz = () => {
+    return (
+        <React.Fragment>
+            {/* <div className='quiz-main d-flex justify-content-center align-items-center'>
+                <span className='d-flex align-items-center'>
+
+                coming inside
+                </span>
+            </div> */}
+        </React.Fragment>
+    )
+}
+
+export default Quiz;
